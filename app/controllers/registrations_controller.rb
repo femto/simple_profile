c0e4db1d406b2ed7776a90dcbf813096d5e92df4
femto <femtowin@gmail.com>
2013-09-21 11:57:46 +0800

    account_update_params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params[:user].permit(:email, :password, :password_confirmation, :avatar,:avatar_cache)
   end
+  def account_update_params
+    params[:user].permit(:email, :password, :password_confirmation, :current_password,:avatar,:avatar_cache)
+  end
 end
